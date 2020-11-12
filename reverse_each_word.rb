7a1fdccb9b3ba99a135d049d6a1b8dd929d17813
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-12 22:36:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,11 @@
 
 
 def reverse_each_word(string)
-  temp_array = string.split(" ")
-  temp_array.collect { |element| }
-  
-  temp_array.join(" ")
-  
-end
-
-def reverse_each_word(string)
   temp_array = []
   temp_array = string.split(" ")
   reverse_array = []
-  temp_array.each { |element| reverse_array.push(element.reverse) } 
+  temp_array.collect { |element| reverse_array.push(element.reverse) } 
 
 reverse_array.join(" ")
+
+end
